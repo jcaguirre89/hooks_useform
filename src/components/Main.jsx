5444e40bb35b52ddedb533cf8bd03eca74efa35c
Cristobal Aguirre <34848638+jcaguirre89@users.noreply.github.com>
2019-04-07 09:08:26 -0700

    FIxed age validation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,8 @@ import * as Yup from "yup";
 const initialValues = {
   name: "",
   email: "",
-  password: ""
+  password: "",
+  age: ""
 };
 
 const validationSchema = Yup.object().shape({
@@ -14,29 +15,17 @@ const validationSchema = Yup.object().shape({
   email: Yup.string()
     .email()
     .required("Required"),
+  age: Yup.number()
+    .typeError("Invalid number")
+    .positive("Must be a positive number")
+    .required("Required"),
   password: Yup.string()
-});
-
-const initialValuesB = {
-  Country: "",
-  Age: ""
-};
-
-const validationSchemaB = Yup.object().shape({
-  Country: Yup.string().required("Required"),
-  Age: Yup.number("Must be a number")
-    .positive("Must be positve")
-    .required("Required")
 });
 
 function Main() {
   return (
     <>
       <Form initialValues={initialValues} validationSchema={validationSchema} />
-      <Form
-        initialValues={initialValuesB}
-        validationSchema={validationSchemaB}
-      />
     </>
   );
 }
